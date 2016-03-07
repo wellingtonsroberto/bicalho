081d5b2484a2161fbc632ef26fdcfd4e62a14d08
Wellington Silvestre <wellingtonsroberto@gmail.com>
2016-03-07 00:00:21 -0300

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[,] vetor1 = new int [5,3];  
+            
+            for (int i = 0; i < 5; i++)
+
+              {
+                    
+             for (int j = 0; j < 3; j++)
+			                      
+              {
+			 
+			  Console.WriteLine ("Informe o valor para linha {0} coluna {1}" , i + 1,j +1);
+                 vetor1[i,j] = Convert.ToInt32(Console.ReadLine());
+
+                   }     
+                }
+               
+            Console.WriteLine("Precione qualquer tecla para mostrar os valores....");
+            Console.ReadKey();
+
+            for (int i = 0; i < 5; i++)
+
+              {
+                    
+             for (int j = 0; j < 3; j++)
+			                      
+              {
+			 
+			  Console.WriteLine ("O valor da linha {0} coluna {1}" , i + 1,j + 1);
+              Console.WriteLine("{0}", vetor1[i, j]);
+                 
+
+                   }     
+                }
+
+                 Console.WriteLine("Precione qualquer tecla para sair !");
+                 Console.ReadKey();
+               
+        
+        }
+    }
+}
+
---
+++ Tabuada.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[,] vetor = new int[10, 2]; 
+            Console.WriteLine("Digite o numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+
+                vetor[i, 0] = i + 1;
+                vetor[i, 1] = num;
+
+            }
+
+            for (int y = 0; y < 10; y++)
+                Console.WriteLine("{0} X {1} = {2}", vetor[y, 0], vetor[y, 1], (vetor[y, 0] * vetor[y, 1]));
+
+            Console.ReadKey();
+
+        
+        
+        }
+    }
+}
